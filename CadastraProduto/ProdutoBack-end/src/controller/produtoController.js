5980efb093254ec7a259f0cf69df3f286afdddc0
Guilherme Xavier <131989419+Xavier4290@users.noreply.github.com>
2026-05-23 18:26:09 -0300

    Update produtoController.js

--- CadastraProduto/ProdutoBack-end/src/controller/produtoController.js
+++ CadastraProduto/ProdutoBack-end/src/controller/produtoController.js
@@ -6,34 +6,31 @@ class ProdutoController {
 
      static async criarProduto(req, res) {
         try {
-
             const produtos = req.body
-
+             
             const produto = await prisma.produto.createMany({
                 data:produtos
             })
+             
             res.status(201).json(produto)
 
         } catch (error) {
-
             console.log(error)
             res.status(500).json({ erro: error.message })
         }
-
-
     }
 
     static async buscarProduto(req, res) {
         const id = Number(req.params.id)
-
         const produtos = await prisma.produto.findMany();
-
+         
         res.json(produtos)
+         
     }
 
     static async buscarProdutoPorId(req, res) {
         const id = Number(req.params.id)
-
+         
         const produto = await prisma.produto.findUnique({
             where: {id}
         })
