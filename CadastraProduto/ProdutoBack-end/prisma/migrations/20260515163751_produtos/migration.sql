9c4eb4e696ff5cdc3096d9ce72bfcb768dbbcbb4
Guilherme Xavier <131989419+Xavier4290@users.noreply.github.com>
2026-05-27 11:53:41 -0300

    Delete CadastraProduto/ProdutoBack-end directory

--- CadastraProduto/ProdutoBack-end/prisma/migrations/20260515163751_produtos/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Produto` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `marca` VARCHAR(191) NOT NULL,
-    `nome` VARCHAR(191) NOT NULL,
-    `data_criacao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- CadastraProduto/ProdutoBack-end/server.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import app from './src/app.js'
-
-
-const PORT = process.env.PORT
-
-app.listen(PORT, ()=> {
-    console.log("Servidor conectado na porta 3001")
-})
